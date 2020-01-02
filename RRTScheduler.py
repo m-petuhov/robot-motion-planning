e7c09265016081dfb6512c516a2c36d3dbd6d42f
Daniel Kunis <dankunis@gmail.com>
2020-01-02 16:36:54 +0300

    Fix checking if line collides with a circle

--- RRTScheduler.py
+++ RRTScheduler.py
@@ -19,13 +19,29 @@ def collides_point(point, circles):
     return False
 
 
+def line_intersects_circle(ax, ay, bx, by, cx, cy, r):
+    ax -= cx
+    ay -= cy
+    bx -= cx
+    by -= cy
+    a = math.pow(bx - ax, 2) + math.pow(by - ay, 2)
+    b = 2*(ax*(bx - ax) + ay*(by - ay))
+    c = math.pow(ax, 2) + math.pow(ay, 2) - math.pow(r, 2)
+    disc = math.pow(b, 2) - 4*a*c
+
+    if(disc <= 0):
+        return False
+    sqrtdisc = math.sqrt(disc)
+    t1 = (-b + sqrtdisc)/(2*a)
+    t2 = (-b - sqrtdisc)/(2*a)
+    if((0 < t1 and t1 < 1) or (0 < t2 and t2 < 1)):
+        return True
+    return False
+
+
 def collides_line(start_point, finish_point, circles):
     for circle in circles:
-        k = (finish_point[1] - start_point[1]) / (finish_point[0] - start_point[0])
-        b = start_point[1] - k * start_point[0]
-        d = (pow((2 * k * b - 2 * circle['X'] - 2 * circle['Y'] * k), 2) - (4 + 4 * k ** 2) * (
-                    b ** 2 - (circle['R']) ** 2 + (circle['X']) ** 2 + circle['Y'] ** 2 - 2 * circle['Y'] * b))
-        if d >= 0:
+        if line_intersects_circle(start_point[0], start_point[1], finish_point[0], finish_point[1], circle['X'], circle['Y'], circle['R']):
             return True
 
     return False
@@ -109,7 +125,7 @@ class RRTScheduler:
             pygame.display.update()
             clock.tick(10000)
 
-
-trivial = Parser('Data/cheese.json')
-scheduler = RRTScheduler(trivial, 5000, 10)
-scheduler.fit()
+if __name__ == "__main__":
+    trivial = Parser('Data/mess.json')
+    scheduler = RRTScheduler(trivial, 5000, 10)
+    scheduler.fit()
